ec81c5140ea661ee11a9b1f48263536b70a28959
Maribel Melendez <lizbethmelendez@live.com>
2021-08-28 17:00:12 -0500

    Set title and menu btn in the same line

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
-$(window).height();
-$(window).width();
-
 
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
@@ -13,17 +10,3 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 })
-
-(document).ready(function(){
-    if(window.innerWidth < 1340){
-      ('#icon').addClass('backup-btn-group').removeClass('btn-group');
-    }
-  });
-  
-(window).resize(function(){
-    if(window.innerWidth < 1340){
-      ('.btn-group').addClass('backup-btn-group').removeClass('btn-group');
-    }else{
-      ('.backup-btn-group').addClass('btn-group').removeClass('backup-btn-group');
-    }
-  });
